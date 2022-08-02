ec940d0f295243c122bb05e7fa5cb3a9bac1c351
David Li <davidli012345@gmail.com>
2022-08-01 22:32:48 -0700

    fix: output logs

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from icecream import ic, colorizedStderrPrint
 LOG_FILE = "log.txt"
 def writeToLog(s):
     # write to log file
-    # colorizedStderrPrint(s)
+    colorizedStderrPrint(s)
     with open(LOG_FILE, "a") as f:
         f.write(s)
         f.write("\n")
