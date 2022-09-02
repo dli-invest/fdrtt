6cb17929a121156cc173afbaf20ab8b6684a91d9
David Li <davidli012345@gmail.com>
2022-09-02 15:39:31 -0700

    Update dispatch_another_event.py

--- dispatch_another_event.py
+++ dispatch_another_event.py
@@ -34,7 +34,7 @@ def dispatch_github_event(args: dict):
 
     if args.get("table_name"):
         data["inputs"]["table_name"] = args.get("table_name")
-
+    print("ATTEMPTING TO RERUN SCRIPT"
     r = requests.post(url, headers=headers, json=data)
     print(r.text)
     print(r.status_code)
